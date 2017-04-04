2494babe7b8c29dd641be79fdfb6bc2e1e4e7ea0
niowa <andres7480311@mail.ru>
2017-04-04 15:22:13 +0300

    simple-game/feature: implement more obsticles

--- src/game.js
+++ src/game.js
@@ -7,28 +7,42 @@ Physijs.scripts.worker = 'vendor/physijs/physijs_worker.js';//unavoidable nail
 
 
 window.game.core = function (component) {
+    var obsticles = {};
+    var waves = 3;
+    var obI = 0,
+        obJ = 1;
+    var first = true;
+    var KEYDOWN = 200;
     var scene = new Physijs.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
     controls = new OrbitControls(camera);
     var renderer = new THREE.WebGLRenderer({ antialias: true });
     var isPaused = false;
-    var sceneSpeed = 1 ;
+    var sceneSpeed = 3 ;
     var _game = {
         init:function () {
             _level.initWorld(window.innerWidth,window.innerHeight);
             _level.initPersona();
+            _level.initGO();
             _level.initLVL(window.innerWidth,window.innerHeight);
             _game._loop();
         },
         _loop:function () {
             requestAnimationFrame( _game._loop );
 
+            for (let i = 0, j = 1; i < Object.keys(obsticles).length; i = i + 2, j = j + 2) {
+              obsticles[i].__dirtyPosition = true;
+              obsticles[j].__dirtyPosition = true;
+              obsticles[i].position.x-=sceneSpeed;
+              obsticles[j].position.x-=sceneSpeed;
 
-            obsticle1.__dirtyPosition = true;
-            obsticle2.__dirtyPosition = true;
-            obsticle1.position.x-=sceneSpeed;
-
-            obsticle2.position.x-=sceneSpeed;
+            }
+            //
+            // obsticleUpper.__dirtyPosition = true;
+            // obsticleLower.__dirtyPosition = true;
+            // obsticleUpper.position.x-=sceneSpeed;
+            //
+            // obsticleLower.position.x-=sceneSpeed;
 
             _level._outOfBound(1600,900);
             if (!isPaused) {
@@ -48,7 +62,6 @@ window.game.core = function (component) {
             scene.setGravity(new THREE.Vector3( 0,-250, 0 ));
             camera.position.z = 500;
 
-
             const  galaxyTexture	= THREE.ImageUtils.loadTexture('resources/galaxy_starfield.png');
             let material	= new THREE.MeshBasicMaterial({
                 map	: galaxyTexture,
@@ -62,16 +75,19 @@ window.game.core = function (component) {
             light.position.set(100, 0, 250);
             scene.add(light);
         },
-        initPersona:function () {
+        initPersona: function () {
             let geometry = new THREE.CubeGeometry(100,100,100);
             let material = Physijs.createMaterial(  new THREE.MeshLambertMaterial({
                 color: 'white',
             }),.8,.3);
 
-
             window.persona = new Physijs.BoxMesh(geometry,material);
             persona.addEventListener( 'collision', function functionName() {
                 _level._gameOver();
+
+
+                //alert("oops");
+
             });
             persona.position.z=60;
             persona.position.x=-400;
@@ -79,38 +95,101 @@ window.game.core = function (component) {
 
             window.addEventListener('keydown',function () {
                 persona.setAngularVelocity({x: 0, y: 0, z: 0});
-                var effect = new THREE.Vector3(0,350,0);
+                var effect = new THREE.Vector3(0,KEYDOWN,0);
                 persona.setLinearVelocity(effect);
             });
         },
+
+        initGO: function () {
+            // window.invisibleLine = new Physijs.PlaneMesh(
+            //     new THREE.PlaneGeometry(1, 900, 1),
+            //     Physijs.createMaterial(new THREE.MeshBasicMaterial({color: 0x00ffff, side: THREE.DoubleSide}),.8,.3)
+            // );
+            let geometry = new THREE.CubeGeometry(0.1,900,0.1);
+            let material = Physijs.createMaterial(  new THREE.MeshLambertMaterial({
+                color: 'black',
+            }),.8,.3);
+
+            window.invisibleLine = new Physijs.BoxMesh(geometry,material, 200);
+
+            invisibleLine.position.x = - window.innerHeight / 2 - 550;
+            invisibleLine.position.z = 50;
+            invisibleLine.addEventListener( 'collision', function functionName() {
+              //if (lastObsticle <
+              _level.deleteObsticles();
+              _level.initLVL(window.innerWidth,window.innerHeight);
+
+
+              //  waves = waves + 2;
+                //alert("hello");
+
+            });
+            scene.add(invisibleLine);
+
+            window.addEventListener('keydown',function () {
+                invisibleLine.setAngularVelocity({x: 0, y: 0, z: 0});
+                var effect = new THREE.Vector3(0,KEYDOWN,0);
+                invisibleLine.setLinearVelocity(effect);
+            });
+
+        },
+
         initLVL:function (width,height) {
-           spawnObsticles(120);
+          if (first) {
+            for ( ; obI < waves * 2; obI += 2, obJ += 2) {
+              spawnObsticles(400, obI, obJ, 300);
+              renderer.setSize( window.innerWidth, window.innerHeight );
+              document.body.appendChild( renderer.domElement );
+              first = false;
+            }
+          } else {
+            obI += 2;
+            obJ += 2;
+            if (obI > waves * 2 - 1) {
+              obI = 0;
+              obJ = 1;
 
+            }
+            console.log(obI);
+            spawnObsticles(400, obI, obJ);
 
-
-            renderer.setSize( window.innerWidth, window.innerHeight );
-            document.body.appendChild( renderer.domElement );
-
+          }
 
             //#TODO:niowa
-            function spawnObsticles(gap) {
-                geometry = new THREE.CubeGeometry(100,window.innerHeight/2,100);
+            function spawnObsticles(gap, i, j, distance = 0) {
+                const upperHeight = Math.random() * (height - gap - 100 - 100) + 100;
+                const lowerPosition = height / 2 - upperHeight - gap;
+                const lowerHeight = Math.abs(-height / 2 - lowerPosition);
+
+                geometry = new THREE.CubeGeometry(100, upperHeight, 100);
                 material = Physijs.createMaterial(  new THREE.MeshLambertMaterial({
                     color: 'white',
                     // wireframe: true
                 }),.8,.3);
 
-                obsticle1 = new Physijs.BoxMesh(geometry,material,0);
-                obsticle1.position.y=height/2;
-                obsticle1.position.z = 50;
-                scene.add(obsticle1);
+                obsticleUpper = new Physijs.BoxMesh(geometry,material,0);
+                obsticleUpper.position.x = width / 2 + 200 + distance * i;
+                obsticleUpper.position.y= height/2 - (upperHeight / 2);
+                obsticleUpper.position.z = 50;
+                scene.add(obsticleUpper);
 
-                obsticle2 = new Physijs.BoxMesh(geometry,material,0);
-                obsticle2.position.y=-height/2;
-                obsticle2.position.x=width/2+25;
-                obsticle2.position.z = 50;
-                scene.add(obsticle2);
+                geometry = new THREE.CubeGeometry(100, lowerHeight, 100);
+                obsticleLower = new Physijs.BoxMesh(geometry,material,0);
+                obsticleLower.position.x = width / 2 + 200 + distance * i;
+                obsticleLower.position.y= lowerPosition - lowerHeight / 2 ;
+                obsticleLower.position.z = 50;
+                scene.add(obsticleLower);
+                obsticles[i] = obsticleUpper;
+                obsticles[j] = obsticleLower;
             }
+        },
+        deleteObsticles: function(obj) {
+            var allChildren = scene.children;
+            var lastObject = allChildren[allChildren.length-waves * 2];
+            var prevLastrObject = allChildren[allChildren.length-waves * 2 + 1];
+            scene.remove(lastObject);
+            scene.remove(prevLastrObject);
+            // scene.remove(obj);
         },
         _gameOver:function() {
         if (isPaused) return;
@@ -119,7 +198,7 @@ window.game.core = function (component) {
          },
 
         _outOfBound:function (width,height) {
-        if (persona.position.y < -(height / 2 + 50) || persona.position.y > height / 2 + 50)  _level._gameOver();
+        if (persona.position.y < -(height / 2 - 150) || persona.position.y > height / 2 - 150)  _level._gameOver();
     }
     };
     window.addEventListener('resize', function(){
