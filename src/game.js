860ea9f6f83074d5b24903c86cb33a664eb2a810
niowa <andres7480311@mail.ru>
2017-04-05 22:27:03 +0300

    simple-game/fix: fixed add and remove obsticles

--- src/game.js
+++ src/game.js
@@ -7,10 +7,7 @@ Physijs.scripts.worker = 'vendor/physijs/physijs_worker.js';//unavoidable nail
 
 
 window.game.core = function (component) {
-    var obsticles = {};
-    var waves = 3;
-    var obI = 0,
-        obJ = 1;
+    var obsticlesCount = true;
     var first = true;
     var KEYDOWN = 200;
     var scene = new Physijs.Scene();
@@ -29,20 +26,6 @@ window.game.core = function (component) {
         },
         _loop:function () {
             requestAnimationFrame( _game._loop );
-
-            for (let i = 0, j = 1; i < Object.keys(obsticles).length; i = i + 2, j = j + 2) {
-              obsticles[i].__dirtyPosition = true;
-              obsticles[j].__dirtyPosition = true;
-              obsticles[i].position.x-=sceneSpeed;
-              obsticles[j].position.x-=sceneSpeed;
-
-            }
-            //
-            // obsticleUpper.__dirtyPosition = true;
-            // obsticleLower.__dirtyPosition = true;
-            // obsticleUpper.position.x-=sceneSpeed;
-            //
-            // obsticleLower.position.x-=sceneSpeed;
 
             _level._outOfBound(1600,900);
             if (!isPaused) {
@@ -84,10 +67,6 @@ window.game.core = function (component) {
             window.persona = new Physijs.BoxMesh(geometry,material);
             persona.addEventListener( 'collision', function functionName() {
                 _level._gameOver();
-
-
-                //alert("oops");
-
             });
             persona.position.z=60;
             persona.position.x=-400;
@@ -101,95 +80,87 @@ window.game.core = function (component) {
         },
 
         initGO: function () {
-            // window.invisibleLine = new Physijs.PlaneMesh(
-            //     new THREE.PlaneGeometry(1, 900, 1),
-            //     Physijs.createMaterial(new THREE.MeshBasicMaterial({color: 0x00ffff, side: THREE.DoubleSide}),.8,.3)
-            // );
-            let geometry = new THREE.CubeGeometry(0.1,900,0.1);
+            let geometry = new THREE.CubeGeometry(2,900,2);
             let material = Physijs.createMaterial(  new THREE.MeshLambertMaterial({
-                color: 'black',
+                color: 'white',
             }),.8,.3);
 
-            window.invisibleLine = new Physijs.BoxMesh(geometry,material, 200);
+            window.invisibleLine = new Physijs.BoxMesh(geometry,material, 10000);
 
             invisibleLine.position.x = - window.innerHeight / 2 - 550;
             invisibleLine.position.z = 50;
-            invisibleLine.addEventListener( 'collision', function functionName() {
-              //if (lastObsticle <
-              _level.deleteObsticles();
-              _level.initLVL(window.innerWidth,window.innerHeight);
-
-
-              //  waves = waves + 2;
-                //alert("hello");
-
+            var effect = new THREE.Vector3(0,0,0);
+            var effect1 = new THREE.Vector3(0,0,0);
+            invisibleLine.addEventListener( 'collision', function functionName(obj) {
+              _level.deleteObsticles(obj);
+              _level.spawnObsticles(300);
             });
+
+
             scene.add(invisibleLine);
-
-            window.addEventListener('keydown',function () {
-                invisibleLine.setAngularVelocity({x: 0, y: 0, z: 0});
-                var effect = new THREE.Vector3(0,KEYDOWN,0);
-                invisibleLine.setLinearVelocity(effect);
-            });
+            invisibleLine.setAngularFactor(effect);
+            invisibleLine.setLinearFactor(effect);
+            invisibleLine.setLinearVelocity(effect1);
+            invisibleLine.setAngularVelocity(effect);
 
         },
 
         initLVL:function (width,height) {
-          if (first) {
-            for ( ; obI < waves * 2; obI += 2, obJ += 2) {
-              spawnObsticles(400, obI, obJ, 300);
-              renderer.setSize( window.innerWidth, window.innerHeight );
-              document.body.appendChild( renderer.domElement );
-              first = false;
-            }
-          } else {
-            obI += 2;
-            obJ += 2;
-            if (obI > waves * 2 - 1) {
-              obI = 0;
-              obJ = 1;
-
-            }
-            console.log(obI);
-            spawnObsticles(400, obI, obJ);
-
+          for (let i = -1; i < 3; i++) {
+            _level.spawnObsticles(300, 400 * i);
           }
 
+
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            document.body.appendChild( renderer.domElement );
+
             //#TODO:niowa
-            function spawnObsticles(gap, i, j, distance = 0) {
-                const upperHeight = Math.random() * (height - gap - 100 - 100) + 100;
-                const lowerPosition = height / 2 - upperHeight - gap;
-                const lowerHeight = Math.abs(-height / 2 - lowerPosition);
 
-                geometry = new THREE.CubeGeometry(100, upperHeight, 100);
-                material = Physijs.createMaterial(  new THREE.MeshLambertMaterial({
-                    color: 'white',
-                    // wireframe: true
-                }),.8,.3);
-
-                obsticleUpper = new Physijs.BoxMesh(geometry,material,0);
-                obsticleUpper.position.x = width / 2 + 200 + distance * i;
-                obsticleUpper.position.y= height/2 - (upperHeight / 2);
-                obsticleUpper.position.z = 50;
-                scene.add(obsticleUpper);
-
-                geometry = new THREE.CubeGeometry(100, lowerHeight, 100);
-                obsticleLower = new Physijs.BoxMesh(geometry,material,0);
-                obsticleLower.position.x = width / 2 + 200 + distance * i;
-                obsticleLower.position.y= lowerPosition - lowerHeight / 2 ;
-                obsticleLower.position.z = 50;
-                scene.add(obsticleLower);
-                obsticles[i] = obsticleUpper;
-                obsticles[j] = obsticleLower;
-            }
         },
+        spawnObsticles: function (gap, distance = 0) {
+            const upperHeight = Math.random() * (window.innerHeight - gap - 100 - 100) + 100;
+            const lowerPosition = window.innerHeight / 2 - upperHeight - gap;
+            const lowerHeight = Math.abs(-window.innerHeight / 2 - lowerPosition);
+
+            geometry = new THREE.CubeGeometry(100, upperHeight, 100);
+            material = Physijs.createMaterial(  new THREE.MeshLambertMaterial({
+                color: 'white',
+                // wireframe: true
+            }),.8,.3);
+
+            obsticleUpper = new Physijs.BoxMesh(geometry,material);
+            obsticleUpper.position.x = window.innerWidth / 2 + 200 + distance;
+            obsticleUpper.position.y= window.innerHeight/2 - (upperHeight / 2);
+            obsticleUpper.position.z = 50;
+            var effect = new THREE.Vector3(0,0,0);
+            var effect1 = new THREE.Vector3(-200,0,0);
+            scene.add(obsticleUpper);
+            obsticleUpper.setAngularFactor(effect);
+            obsticleUpper.setLinearFactor(effect);
+            obsticleUpper.setLinearVelocity(effect1);
+            obsticleUpper.setAngularVelocity(effect);
+
+            geometry = new THREE.CubeGeometry(100, lowerHeight, 100);
+            obsticleLower = new Physijs.BoxMesh(geometry,material);
+            obsticleLower.position.x = window.innerWidth / 2 + 200 + distance;
+            obsticleLower.position.y= lowerPosition - lowerHeight / 2 ;
+            obsticleLower.position.z = 50;
+            var effect = new THREE.Vector3(0,0,0);
+            var effect1 = new THREE.Vector3(-200,0,0);
+            scene.add(obsticleLower);
+            obsticleLower.setAngularFactor(effect);
+            obsticleLower.setLinearFactor(effect);
+            obsticleLower.setLinearVelocity(effect1);
+            obsticleLower.setAngularVelocity(effect);
+
+            obsticleUpper.together = obsticleLower;
+
+        },
+
+
         deleteObsticles: function(obj) {
-            var allChildren = scene.children;
-            var lastObject = allChildren[allChildren.length-waves * 2];
-            var prevLastrObject = allChildren[allChildren.length-waves * 2 + 1];
-            scene.remove(lastObject);
-            scene.remove(prevLastrObject);
-            // scene.remove(obj);
+            scene.remove(obj);
+            scene.remove(obj.together);
         },
         _gameOver:function() {
         if (isPaused) return;
